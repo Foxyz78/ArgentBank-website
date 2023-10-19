d8a83f54dd24efc4da71cda0f02f7baad3fc2aec
Foxyz78 <foxyz78@gmail.com>
2023-10-19 09:33:38 +0200

    Conversion React de la page Home

--- front/src/components/Layout.jsx
+++ front/src/components/Layout.jsx
@@ -1,18 +1,17 @@
-
 import React from "react";
 
 import Header from "./Header";
+import Home from "../pages/Home";
 import Footer from "./Footer";
 
-const Layout = () => {
+const Layout = ({ children }) => {
     return (
         <>
             <Header />
-
+            <main>{children}</main>
             <Footer />
         </>
     );
 };
-
 
 export default Layout;
