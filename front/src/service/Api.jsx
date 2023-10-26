41b957335b7a4ad53fb9a168892f73a9b95206c9
Foxyz78 <foxyz78@gmail.com>
2023-10-26 10:52:53 +0200

    création de la connexion à l'api

---
+++ front/src/service/Api.jsx
@@ -0,0 +1,53 @@
+const baseUrl = "localhost:3001/api/v1";
+
+const fetchData = {
+    getToken: {
+        url: "/user/login",
+        method: "post",
+        auth: false,
+    },
+    getProfile: {
+        url: "/user/profile",
+        method: "post",
+        auth: true,
+    },
+    putUserName: {
+        url: "/user/profile",
+        method: "put",
+        auth: true,
+    },
+};
+
+const Api = async (info, token, data = {}) => {
+    const apiData = fetchData[info];
+
+    if (!apiData) {
+        console.error("erreur de connexion");
+        return;
+    }
+
+    const headers = { "Content-Type": "application/json" };
+
+    if (!apiData.auth) {
+        headers.Authorization = `Bearer ${token}`;
+    }
+
+    try {
+        const response = await fetch(`${baseUrl}${apiData.url}`, {
+            method: apiData.method,
+            headers,
+            body: JSON.stringify(data),
+        });
+        if (!response.ok) {
+            console.log(response);
+            const errorData = await response.json();
+            throw new Error(errorData.message);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Erreur lors de la connexion à l'API :", error);
+        throw error;
+    }
+};
+
+export default Api;
