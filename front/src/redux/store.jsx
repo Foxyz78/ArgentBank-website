d53b1bbdf3dffff37e13ad8c307aceb417fa8f31
Foxyz78 <foxyz78@gmail.com>
2023-10-27 08:58:19 +0200

    Provider ajouté dans index.js

--- front/src/redux/store.jsx
+++ front/src/redux/store.jsx
@@ -1,1 +1,12 @@
-import { createstore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import signInSlice from "./signInSlice";
+
+const store = configureStore({
+    reducer: {
+        signIn: signInSlice.reducer,
+    },
+});
+
+export const { signIn, signOut } = signInSlice.actions;
+
+export default store;
